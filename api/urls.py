fdfeec6417dd9fe3d1d908924ecc17123df40c69
vishnu <vishnuvijayan123abc@gmail.com>
2024-01-09 10:25:40 +0530

    second

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from api import views
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.routers import DefaultRouter
+
+router=DefaultRouter
+router.register("products",views.ProductView,basename="product")
 
 urlpatterns = [
     path('register/',views.SignUpView.as_view()),
     path('token/',ObtainAuthToken.as_view())
     
-]
+]+router.urls
